79e95686283e37ed5e09e91020b5377bea813e79
Anna Potapova <terpigoreva1992@gmail.com>
2025-08-20 10:57:51 +0300

    added fake API, users component, bootstrap

---
+++ src/app/components/users.jsx
@@ -0,0 +1,9 @@
+import api from '../api';
+
+const Users = () => {
+    console.log(api.users.fetchAll());
+
+    return <h1>Users</h1>;
+};
+
+export default Users;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
+import Users from './components/users';
+
 export default function Home() {
-    return (
-        <div>
-            <main></main>
-        </div>
-    );
+    return <Users />;
 }
