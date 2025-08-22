c2a9af054d7430870a14c0adfd3578d8443b357c
Anna Potapova <terpigoreva1992@gmail.com>
2025-08-22 09:43:22 +0300

    fixed the redirection to the penultimate page if there are no users left on the last page

--- src/app/components/pagination.jsx
+++ src/app/components/pagination.jsx
@@ -1,7 +1,17 @@
 import _ from 'lodash';
+import { useEffect } from 'react';
 
 const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
     const pageCount = Math.ceil(itemsCount / pageSize);
+
+    useEffect(() => {
+        if (currentPage > pageCount && pageCount > 0) {
+            onPageChange(pageCount);
+        } else if (itemsCount === 0 && currentPage !== 1) {
+            onPageChange(1);
+        }
+    }, [itemsCount, pageCount, currentPage, onPageChange]);
+
     if (pageCount === 1) return null;
     const pages = _.range(1, pageCount + 1);
 
@@ -13,9 +23,9 @@ const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
                         className={'page-item' + (page === currentPage ? ' active' : '')}
                         key={'page_' + page}
                     >
-                        <a className="page-link" onClick={() => onPageChange(page)}>
+                        <button className="page-link" onClick={() => onPageChange(page)}>
                             {page}
-                        </a>
+                        </button>
                     </li>
                 ))}
             </ul>
