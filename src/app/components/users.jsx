46d4c3ca6450a94c6214e005d4d19b14d33e96f4
Anna Potapova <terpigoreva1992@gmail.com>
2025-08-22 08:28:09 +0300

    added pagination

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+import Pagination from './pagination';
 import User from './user';
+import { paginate } from '../utils/paginate';
 
 const Users = ({ users, ...rest }) => {
+    const count = users.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const handlePageChange = pageIndex => {
+        setCurrentPage(pageIndex);
+    };
+
+    const userCrop = paginate(users, currentPage, pageSize);
     return (
         <>
-            {users.length > 0 && (
+            {count > 0 && (
                 <table className="table">
                     <thead>
                         <tr>
@@ -16,12 +27,18 @@ const Users = ({ users, ...rest }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map(user => (
+                        {userCrop.map(user => (
                             <User key={user._id} {...user} {...rest} />
                         ))}
                     </tbody>
                 </table>
             )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                onPageChange={handlePageChange}
+                currentPage={currentPage}
+            />
         </>
     );
 };
