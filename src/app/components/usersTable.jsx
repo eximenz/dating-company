75820803b5800f832dafd0c82cb37b734ae2c904
Anna Potapova <terpigoreva1992@gmail.com>
2025-08-26 14:06:16 +0300

    extracted the table body into a component

--- src/app/components/usersTable.jsx
+++ src/app/components/usersTable.jsx
@@ -1,24 +1,26 @@
+import TableBody from './tableBody';
 import TableHeader from './tableHeader';
-import User from './user';
+// import User from './user';
 
 const UsersTable = ({ users, onSort, selectedSort, ...rest }) => {
     const columns = {
-        name: { iter: 'name', name: 'Имя' },
+        name: { path: 'name', name: 'Имя' },
         qualities: { name: 'Качества' },
-        professions: { iter: 'profession.name', name: 'Профессия' },
-        completedMeetings: { iter: 'completedMeetings', name: 'Встретился раз' },
-        rate: { iter: 'rate', name: 'Оценка' },
-        bookmark: { iter: 'bookmark', name: 'Избранное' },
+        professions: { path: 'profession.name', name: 'Профессия' },
+        completedMeetings: { path: 'completedMeetings', name: 'Встретился раз' },
+        rate: { path: 'rate', name: 'Оценка' },
+        bookmark: { path: 'bookmark', name: 'Избранное' },
         delete: {},
     };
     return (
         <table className="table">
             <TableHeader {...{ onSort, selectedSort, columns }} />
-            <tbody>
+            <TableBody {...{ columns, data: users }} />
+            {/* <tbody>
                 {users.map(user => (
                     <User key={user._id} {...user} {...rest} />
                 ))}
-            </tbody>
+            </tbody> */}
         </table>
     );
 };
